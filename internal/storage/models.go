25350734984433c4c6cf587362b03af1f05f78de
Andrey Tinyakov <akoff16@gmail.com>
2025-04-22 23:15:16 +0200

    moooar godoc

--- internal/storage/models.go
+++ internal/storage/models.go
@@ -1,9 +1,15 @@
+// Package storage provides data models and functions for managing URL records.
+// It contains the URLRecord struct, which represents a record for a shortened URL,
+// including the original URL, shortened URL, associated user ID, and a deletion flag.
 package storage
 
+// URLRecord represents a record for a shortened URL in the storage system.
+// It contains the original URL, the shortened URL, the user ID who created the record,
+// and a flag indicating whether the record is marked as deleted.
 type URLRecord struct {
-	ID        string `json:"uuid"`
-	Original  string `json:"original_url"`
-	Short     string `json:"short_url"`
-	UserID    string `json:"user_id"`
-	IsDeleted bool   `json:"is_deleted"`
+	ID        string `json:"uuid"`         // The unique identifier for the URL record
+	Original  string `json:"original_url"` // The original URL before shortening
+	Short     string `json:"short_url"`    // The shortened URL
+	UserID    string `json:"user_id"`      // The ID of the user who created the shortened URL
+	IsDeleted bool   `json:"is_deleted"`   // A flag indicating if the URL record is deleted
 }
